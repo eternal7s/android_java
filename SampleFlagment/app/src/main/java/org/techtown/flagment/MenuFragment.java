238369efc4d7e8b63b323a49915762b3554abbe5
eternal7s <eternal7s@naver.com>
2021-04-20 23:05:15 +0900

    20210420 study

---
+++ SampleFlagment/app/src/main/java/org/techtown/flagment/MenuFragment.java
@@ -0,0 +1,24 @@
+package org.techtown.flagment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.fragment.app.Fragment;
+
+public class MenuFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_menu, container, false);
+        Button button = rootView.findViewById(R.id.button);
+        button.setOnClickListener(v -> {
+            MainActivity activity = (MainActivity) getActivity();
+            activity.onFragmentChanged(1);
+        });
+        return rootView;
+    }
+}
