8896cc60a6dfcd2c268d504fdca8583e0149e180
eternal7s <eternal7s@naver.com>
2021-06-02 00:02:02 +0900

    20210601

---
+++ DoitMission/DoitMission13/app/src/main/java/org/techtown/doitmission13/OnCustomerItemClickListener.java
@@ -0,0 +1,7 @@
+package org.techtown.doitmission13;
+
+import android.view.View;
+
+public interface OnCustomerItemClickListener {
+    public void onItemClick(CustomerAdapter.ViewHolder holder, View view, int position);
+}
